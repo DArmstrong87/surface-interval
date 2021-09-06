a818ab2b24edfd61fb4dc4975d0801b8f7193cf2
DArmstrong87 <lenzflare@gmail.com>
2021-09-06 16:16:45 -0500

    installed sweet alert for login form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,12 +1,30 @@
 import React from "react";
 import "./Login.css"
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+const MySwal = withReactContent(Swal)
+
+const LoginForm = () => {
+    MySwal.fire({
+        title: "Login",
+        didOpen: () => {
+            MySwal.clickConfirm()
+        }
+    }).then(() => {
+        return MySwal.fire(<><h2>Login</h2>
+            <input type="text" placeholder="username"></input>
+            <input type="text" placeholder="password"></input>
+            </>)
+    })
+}
 
 export const Login = () => {
     return (<>
         <section className="loginForm">
             <h1>Surface Interval</h1>
             <div className="loginDiv">
-                <button className="loginButton">Login</button>
+                <button className="loginButton" onClick={LoginForm}>Login</button>
                 <button className="createUserButton">Create User</button>
             </div>
         </section>
--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { SurfaceInterval } from './SurfaceInterval';
+// import { SurfaceInterval } from './SurfaceInterval';
 import { BrowserRouter } from 'react-router-dom'
+import { Login } from './components/auth/Login';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <SurfaceInterval />
+    <Login />
+      {/* <SurfaceInterval /> */}
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
