0740f25f473cd5ecf7018547e7ececf3b518bcdc
DArmstrong87 <lenzflare@gmail.com>
2021-09-07 22:00:46 -0500

    fixed scrolling background, added initial code for gear module

--- src/components/application/ApiManager.js
+++ src/components/application/ApiManager.js
@@ -3,7 +3,23 @@ export const getMyDives = () => {
     return fetch(`http://localhost:8088/dives?&userId=${user}`)
         .then(res => res.json())
 }
+export const getMyGear = () => {
+    const user = localStorage.getItem('si_user')
+    return fetch(`http://localhost:8088/gear?&userId=${user}`)
+        .then(res => res.json())
+}
 export const getCurrentDive = (id) => {
     return fetch(`http://localhost:8088/dives/${id}`)
         .then(res => res.json())
+}
+
+export const deleteDive = (id) => {
+    return fetch(`http://localhost:8088/dives/${id}`, {
+        method: "DELETE"
+    })
+}
+export const deleteGear = (id) => {
+    return fetch(`http://localhost:8088/gear/${id}`, {
+        method: "DELETE"
+    })
 }
--- src/components/gear/Gear.js
+++ src/components/gear/Gear.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react"
+import { deleteGear, getMyGear } from "../application/ApiManager"
 
 export const Gear = () => {
+    const [gear, setGear] = useState([])
+
+    useEffect(
+        () => {
+            getMyGear()
+                .then(gear => {
+                    setGear(gear)
+                })
+        },
+        []
+    )
+
     return (<>
         <article className='diveLogArticle'>
             <section className='diveLogHeading'>
                 <h2 className='moduleTitle'>Gear</h2>
+                <button className="logDive">
+                    Create Gear Set<br />
+                    🤿
+                </button>
             </section>
         </article>
 
-
+        <article className='diveLogMain' key='article2'>
+            {gear.map(
+                (gearset) => {
+                    return <section className='diveLogEntry' key={`section--${gear.id}`}><div>
+                        <h2 className='diveNumber'>{gearset.name}</h2>
+                        <ul className='diveList'>
+                            <li><b>BCD:</b> {gearset.bcd}</li>
+                            <li><b>Regulator:</b> {gearset.regulator}</li>
+                        </ul>
+                    </div>
+                        <div className='edit-delete'>
+                            <a href={`/gear/edit/${gear.id}`}>Edit</a>
+                            <a href="##" onClick={() => {
+                                deleteGear(gear.id)
+                            }}>Delete</a>
+                        </div>
+                    </section>
+                }
+            )}
+        </article>
     </>)
 }
