7ce3ee7bf5951dc4d51ae5322092ebfc6c1fc9bf
DArmstrong87 <lenzflare@gmail.com>
2021-09-10 10:51:41 -0500

    added initial elements for profile page

--- src/components/profile/DiverProfile.js
+++ src/components/profile/DiverProfile.js
@@ -1,10 +1,41 @@
 import React from "react";
+import "./DiverProfile.css"
 
 export const DiverProfile = () => {
     return (<>
         <article className='diveLogArticle'>
             <section className='diveLogHeading'>
-                <h2 className='moduleTitle'>Profile</h2>
+                <h2 className='moduleTitle'>Diver Profile</h2>
+            </section>
+        </article>
+
+        <article className='diverProfileContainer'>
+            <section className='diverStats'>
+                <ul>
+                    <li>Name</li>
+                    <li>Highest Cert Level: </li>
+                    <li>Total Dives: </li>
+                    <li>Most recent dive:</li>
+                    <li>Deepest Dive</li>
+                    <li>Longest Dive</li>
+                </ul>
+            </section>
+            <div className="profilePic">
+                <img src="/images/da-upside-down.jpg" alt="profile pic" />
+            </div>
+        </article>
+
+        <div>
+            <button className="logDive">Upload Cert Card</button>
+        </div>
+
+        <article className="certCards">
+            <h2 className="moduleTitle">Certification Cards</h2>
+            <section className="certCardContainer">
+                <div className="certCard"></div>
+                <div className="certCard"></div>
+                <div className="certCard"></div>
+                <div className="certCard"></div>
             </section>
         </article>
     </>)
