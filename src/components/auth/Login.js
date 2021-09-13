6da75856434ffe9ee026987d98ce487b23b87d55
DArmstrong87 <lenzflare@gmail.com>
2021-09-13 14:55:00 -0500

    made the styling for the login and register pages realll niiice

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,38 +1,7 @@
 import React, { useRef, useState } from "react";
 import "./Login.css"
-// import Swal from 'sweetalert2'
-// import withReactContent from 'sweetalert2-react-content'
 import { useHistory } from "react-router";
 import { Link } from "react-router-dom";
-import "./Login.css"
-
-
-// const LoginForm = () => {
-//     MySwal.fire({
-//         title: "Login",
-//         didOpen: () => {
-//             MySwal.clickConfirm()
-//         }
-//     }).then(() => {
-//         return MySwal.fire(<><h2>Login</h2>
-//             <input type="text" placeholder="username"></input>
-//             <input type="text" placeholder="password"></input>
-//         </>)
-//     })
-// }
-
-// export const Login = () => {
-//     return (<>
-//         <section className="loginForm">
-//             <h1>Surface Interval</h1>
-//             <div className="loginDiv">
-//                 <button className="loginButton" onClick={LoginForm}>Login</button>
-//                 <button className="createUserButton">Create User</button>
-//             </div>
-//         </section>
-//     </>
-//     )
-// }
 
 export const Login = () => {
     const [email, set] = useState("")
@@ -60,17 +29,16 @@ export const Login = () => {
 
     return (
         <main className="container--login">
+            <div className="h1-div"><h1>Surface Interval</h1></div>
             <dialog className="dialog dialog--auth" ref={existDialog}>
                 <div>User does not exist</div>
                 <button className="button--close" onClick={e => existDialog.current.close()}>Close</button>
             </dialog>
 
-            <section>
+            <section className="login--field">
                 <form className="form--login" onSubmit={handleLogin}>
-                    <h1>Surface Interval</h1>
                     <h2>Please sign in</h2>
                     <fieldset>
-                        <label htmlFor="inputEmail"> Email address </label>
                         <input type="email"
                             onChange={evt => set(evt.target.value)}
                             className="form-control"
@@ -82,10 +50,10 @@ export const Login = () => {
                             Sign in
                         </button>
                     </fieldset>
+                    <div className="link--register">
+                        <Link to="/register">Not a member yet?</Link>
+                    </div>
                 </form>
-            </section>
-            <section className="link--register">
-                <Link to="/register">Not a member yet?</Link>
             </section>
         </main>
     )
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react"
-import { useHistory } from "react-router-dom"
-import "./Login.css"
+import { Link, useHistory } from "react-router-dom"
+import "./Register.css"
 
 export const Register = (props) => {
     const [user, setUser] = useState({})
@@ -40,34 +40,37 @@ export const Register = (props) => {
     }
 
     const updateuser = (evt) => {
-        const copy = {...user}
+        const copy = { ...user }
         copy[evt.target.id] = evt.target.value
         setUser(copy)
     }
 
     return (
-        <main style={{ textAlign: "center" }}>
+        <main className="register" style={{ textAlign: "center" }}>
+            <div className="h1-div"><h1 className="h3 mb-3 font-weight-normal">Please Register for Surface Interval</h1></div>
             <dialog className="dialog dialog--password" ref={conflictDialog}>
                 <div>Account with that email address already exists</div>
                 <button className="button--close" onClick={e => conflictDialog.current.close()}>Close</button>
             </dialog>
 
-            <form className="form--login" onSubmit={handleRegister}>
-                <h1 className="h3 mb-3 font-weight-normal">Please Register for Kandy Korner</h1>
-                <fieldset>
-                    <label htmlFor="name"> Full Name </label>
-                    <input onChange={updateuser}
-                           type="text" id="name" className="form-control"
-                           placeholder="Enter your name" required autoFocus />
-                </fieldset>
-                <fieldset>
-                    <label htmlFor="email"> Email address </label>
-                    <input onChange={updateuser} type="email" id="email" className="form-control" placeholder="Email address" required />
-                </fieldset>
-                <fieldset>
-                    <button type="submit"> Register </button>
-                </fieldset>
-            </form>
+            <section className="login--field">
+                <form className="form--login" onSubmit={handleRegister}>
+                    <fieldset>
+                        <label htmlFor="name"> Full Name </label>
+                        <input onChange={updateuser}
+                            type="text" id="name" className="form-control"
+                            placeholder="Enter your name" required autoFocus />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="email"> Email address </label>
+                        <input onChange={updateuser} type="email" id="email" className="form-control" placeholder="Email address" required />
+                    </fieldset>
+                    <fieldset>
+                        <button type="submit"> Register </button>
+                        <button onClick={()=> {history.push("/login")}}> Cancel</button>
+                    </fieldset>
+                </form>
+            </section>
         </main>
     )
 }
