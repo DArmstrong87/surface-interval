9e8dcb18d482bef18ae19931a7ff8b049a7d05e7
DArmstrong87 <lenzflare@gmail.com>
2021-09-14 17:52:18 -0500

    refactored functions for efficiency

--- src/components/divePlanner/DiveLogic.js
+++ src/components/divePlanner/DiveLogic.js
@@ -30,91 +30,95 @@ export const DiveStates = () => {
     const dive = { ...dive0 }
     const abt = dive.abt
     const depth = dive.depth
-    const setPG = (letter) => dive.pg = letter;
-    const setSS = (boolean) => dive.ssRequired = boolean;
-    const setNDL = (boolean) => dive.noDecoLimit = boolean;
-    const setDive = (pg, ss, ndl) => { setPG(pg); setSS(ss); setNDL(ndl) };
+    const setAndUpdate = (pg, ss, ndl, dive) => {
+        const setPG = (letter) => dive.pg = letter;
+        const setSS = (boolean) => dive.ssRequired = boolean;
+        const setNDL = (boolean) => dive.noDecoLimit = boolean;
+        const setDive = (pg, ss, ndl) => { setPG(pg); setSS(ss); setNDL(ndl) };
+        setDive(pg, ss, ndl); updateDive1(dive)
+    }
+
 
     const getPressureGroup = () => {
         // Depth <= 35
-        if (abt <= 10 && depth <= 35) { setDive('A', false, false); updateDive1(dive) }
-        else if (abt <= 19 && depth <= 35) { setDive('B', false, false); updateDive1(dive) }
-        else if (abt <= 25 && depth <= 35) { setDive('C', false, false); updateDive1(dive) }
-        else if (abt <= 29 && depth <= 35) { setDive('D', false, false); updateDive1(dive) }
-        else if (abt <= 32 && depth <= 35) { setDive('E', false, false); updateDive1(dive) }
-        else if (abt <= 36 && depth <= 35) { setDive('F', false, false); updateDive1(dive) }
-        else if (abt <= 40 && depth <= 35) { setDive('G', false, false); updateDive1(dive) }
-        else if (abt <= 44 && depth <= 35) { setDive('H', false, false); updateDive1(dive) }
-        else if (abt <= 48 && depth <= 35) { setDive('I', false, false); updateDive1(dive) }
-        else if (abt <= 52 && depth <= 35) { setDive('J', false, false); updateDive1(dive) }
-        else if (abt <= 57 && depth <= 35) { setDive('K', false, false); updateDive1(dive) }
-        else if (abt <= 62 && depth <= 35) { setDive('L', false, false); updateDive1(dive) }
-        else if (abt <= 67 && depth <= 35) { setDive('M', false, false); updateDive1(dive) }
-        else if (abt <= 73 && depth <= 35) { setDive('N', false, false); updateDive1(dive) }
-        else if (abt <= 79 && depth <= 35) { setDive('O', false, false); updateDive1(dive) }
-        else if (abt <= 85 && depth <= 35) { setDive('P', false, false); updateDive1(dive) }
-        else if (abt <= 92 && depth <= 35) { setDive('Q', false, false); updateDive1(dive) }
-        else if (abt <= 100 && depth <= 35) { setDive('R', false, false); updateDive1(dive) }
-        else if (abt <= 108 && depth <= 35) { setDive('S', false, false); updateDive1(dive) }
-        else if (abt <= 117 && depth <= 35) { setDive('T', false, false); updateDive1(dive) }
-        else if (abt <= 127 && depth <= 35) { setDive('U', false, false); updateDive1(dive) }
-        else if (abt <= 139 && depth <= 35) { setDive('V', false, false); updateDive1(dive) }
-        else if (abt <= 152 && depth <= 35) { setDive('W', true, false); updateDive1(dive) }
-        else if (abt <= 168 && depth <= 35) { setDive('X', true, false); updateDive1(dive) }
-        else if (abt <= 188 && depth <= 35) { setDive('Y', true, false); updateDive1(dive) }
-        else if (abt <= 205 && depth <= 35) { setDive('Z', true, true); updateDive1(dive) }
+        if (abt <= 10 && depth <= 35) { setAndUpdate('A', false, false, dive)}
+        else if (abt <= 19 && depth <= 35) { setAndUpdate('B', false, false, dive)}
+        else if (abt <= 25 && depth <= 35) { setAndUpdate('C', false, false, dive)}
+        else if (abt <= 29 && depth <= 35) { setAndUpdate('D', false, false, dive)}
+        else if (abt <= 32 && depth <= 35) { setAndUpdate('E', false, false, dive)}
+        else if (abt <= 36 && depth <= 35) { setAndUpdate('F', false, false, dive)}
+        else if (abt <= 40 && depth <= 35) { setAndUpdate('G', false, false, dive)}
+        else if (abt <= 44 && depth <= 35) { setAndUpdate('H', false, false, dive)}
+        else if (abt <= 48 && depth <= 35) { setAndUpdate('I', false, false, dive)}
+        else if (abt <= 52 && depth <= 35) { setAndUpdate('J', false, false, dive)}
+        else if (abt <= 57 && depth <= 35) { setAndUpdate('K', false, false, dive)}
+        else if (abt <= 62 && depth <= 35) { setAndUpdate('L', false, false, dive)}
+        else if (abt <= 67 && depth <= 35) { setAndUpdate('M', false, false, dive)}
+        else if (abt <= 73 && depth <= 35) { setAndUpdate('N', false, false, dive)}
+        else if (abt <= 79 && depth <= 35) { setAndUpdate('O', false, false, dive)}
+        else if (abt <= 85 && depth <= 35) { setAndUpdate('P', false, false, dive)}
+        else if (abt <= 92 && depth <= 35) { setAndUpdate('Q', false, false, dive)}
+        else if (abt <= 100 && depth <= 35) { setAndUpdate('R', false, false, dive)}
+        else if (abt <= 108 && depth <= 35) { setAndUpdate('S', false, false, dive)}
+        else if (abt <= 117 && depth <= 35) { setAndUpdate('T', false, false, dive)}
+        else if (abt <= 127 && depth <= 35) { setAndUpdate('U', false, false, dive)}
+        else if (abt <= 139 && depth <= 35) { setAndUpdate('V', false, false, dive)}
+        else if (abt <= 152 && depth <= 35) { setAndUpdate('W', true, false, dive)}
+        else if (abt <= 168 && depth <= 35) { setAndUpdate('X', true, false, dive)}
+        else if (abt <= 188 && depth <= 35) { setAndUpdate('Y', true, false, dive)}
+        else if (abt <= 205 && depth <= 35) { setAndUpdate('Z', true, true, dive)}
         // Depth <= 40
-        else if (abt <= 9 && depth <= 40) { setDive('A', false, false); updateDive1(dive) }
-        else if (abt <= 16 && depth <= 40) { setDive('B', false, false); updateDive1(dive) }
-        else if (abt <= 22 && depth <= 40) { setDive('C', false, false); updateDive1(dive) }
-        else if (abt <= 25 && depth <= 40) { setDive('D', false, false); updateDive1(dive) }
-        else if (abt <= 27 && depth <= 40) { setDive('E', false, false); updateDive1(dive) }
-        else if (abt <= 31 && depth <= 40) { setDive('F', false, false); updateDive1(dive) }
-        else if (abt <= 34 && depth <= 40) { setDive('G', false, false); updateDive1(dive) }
-        else if (abt <= 37 && depth <= 40) { setDive('H', false, false); updateDive1(dive) }
-        else if (abt <= 40 && depth <= 40) { setDive('I', false, false); updateDive1(dive) }
-        else if (abt <= 44 && depth <= 40) { setDive('J', false, false); updateDive1(dive) }
-        else if (abt <= 48 && depth <= 40) { setDive('K', false, false); updateDive1(dive) }
-        else if (abt <= 51 && depth <= 40) { setDive('L', false, false); updateDive1(dive) }
-        else if (abt <= 55 && depth <= 40) { setDive('M', false, false); updateDive1(dive) }
-        else if (abt <= 60 && depth <= 40) { setDive('N', false, false); updateDive1(dive) }
-        else if (abt <= 64 && depth <= 40) { setDive('O', false, false); updateDive1(dive) }
-        else if (abt <= 69 && depth <= 40) { setDive('P', false, false); updateDive1(dive) }
-        else if (abt <= 74 && depth <= 40) { setDive('Q', false, false); updateDive1(dive) }
-        else if (abt <= 79 && depth <= 40) { setDive('R', false, false); updateDive1(dive) }
-        else if (abt <= 85 && depth <= 40) { setDive('S', false, false); updateDive1(dive) }
-        else if (abt <= 91 && depth <= 40) { setDive('T', false, false); updateDive1(dive) }
-        else if (abt <= 97 && depth <= 40) { setDive('U', false, false); updateDive1(dive) }
-        else if (abt <= 104 && depth <= 40) { setDive('V', false, false); updateDive1(dive) }
-        else if (abt <= 111 && depth <= 40) { setDive('W', true, false); updateDive1(dive) }
-        else if (abt <= 120 && depth <= 40) { setDive('X', true, false); updateDive1(dive) }
-        else if (abt <= 129 && depth <= 40) { setDive('Y', true, false); updateDive1(dive) }
-        else if (abt <= 140 && depth <= 40) { setDive('Z', true, true); updateDive1(dive) }
+        else if (abt <= 9 && depth <= 40) { setAndUpdate('A', false, false, dive)}
+        else if (abt <= 16 && depth <= 40) { setAndUpdate('B', false, false, dive)}
+        else if (abt <= 22 && depth <= 40) { setAndUpdate('C', false, false, dive)}
+        else if (abt <= 25 && depth <= 40) { setAndUpdate('D', false, false, dive)}
+        else if (abt <= 27 && depth <= 40) { setAndUpdate('E', false, false, dive)}
+        else if (abt <= 31 && depth <= 40) { setAndUpdate('F', false, false, dive)}
+        else if (abt <= 34 && depth <= 40) { setAndUpdate('G', false, false, dive)}
+        else if (abt <= 37 && depth <= 40) { setAndUpdate('H', false, false, dive)}
+        else if (abt <= 40 && depth <= 40) { setAndUpdate('I', false, false, dive)}
+        else if (abt <= 44 && depth <= 40) { setAndUpdate('J', false, false, dive)}
+        else if (abt <= 48 && depth <= 40) { setAndUpdate('K', false, false, dive)}
+        else if (abt <= 51 && depth <= 40) { setAndUpdate('L', false, false, dive)}
+        else if (abt <= 55 && depth <= 40) { setAndUpdate('M', false, false, dive)}
+        else if (abt <= 60 && depth <= 40) { setAndUpdate('N', false, false, dive)}
+        else if (abt <= 64 && depth <= 40) { setAndUpdate('O', false, false, dive)}
+        else if (abt <= 69 && depth <= 40) { setAndUpdate('P', false, false, dive)}
+        else if (abt <= 74 && depth <= 40) { setAndUpdate('Q', false, false, dive)}
+        else if (abt <= 79 && depth <= 40) { setAndUpdate('R', false, false, dive)}
+        else if (abt <= 85 && depth <= 40) { setAndUpdate('S', false, false, dive)}
+        else if (abt <= 91 && depth <= 40) { setAndUpdate('T', false, false, dive)}
+        else if (abt <= 97 && depth <= 40) { setAndUpdate('U', false, false, dive)}
+        else if (abt <= 104 && depth <= 40) { setAndUpdate('V', false, false, dive)}
+        else if (abt <= 111 && depth <= 40) { setAndUpdate('W', true, false, dive)}
+        else if (abt <= 120 && depth <= 40) { setAndUpdate('X', true, false, dive)}
+        else if (abt <= 129 && depth <= 40) { setAndUpdate('Y', true, false, dive)}
+        else if (abt <= 140 && depth <= 40) { setAndUpdate('Z', true, true, dive)}
         // Depth <= 50
-        else if (abt <= 7 && depth <= 50) { setDive('A', false, false); updateDive1(dive) }
-        else if (abt <= 13 && depth <= 50) { setDive('B', false, false); updateDive1(dive) }
-        else if (abt <= 17 && depth <= 50) { setDive('C', false, false); updateDive1(dive) }
-        else if (abt <= 19 && depth <= 50) { setDive('D', false, false); updateDive1(dive) }
-        else if (abt <= 21 && depth <= 50) { setDive('E', false, false); updateDive1(dive) }
-        else if (abt <= 24 && depth <= 50) { setDive('F', false, false); updateDive1(dive) }
-        else if (abt <= 26 && depth <= 50) { setDive('G', false, false); updateDive1(dive) }
-        else if (abt <= 28 && depth <= 50) { setDive('H', false, false); updateDive1(dive) }
-        else if (abt <= 31 && depth <= 50) { setDive('I', false, false); updateDive1(dive) }
-        else if (abt <= 33 && depth <= 50) { setDive('J', false, false); updateDive1(dive) }
-        else if (abt <= 36 && depth <= 50) { setDive('K', false, false); updateDive1(dive) }
-        else if (abt <= 39 && depth <= 50) { setDive('L', false, false); updateDive1(dive) }
-        else if (abt <= 41 && depth <= 50) { setDive('M', false, false); updateDive1(dive) }
-        else if (abt <= 44 && depth <= 50) { setDive('N', false, false); updateDive1(dive) }
-        else if (abt <= 47 && depth <= 50) { setDive('O', false, false); updateDive1(dive) }
-        else if (abt <= 50 && depth <= 50) { setDive('P', false, false); updateDive1(dive) }
-        else if (abt <= 53 && depth <= 50) { setDive('Q', false, false); updateDive1(dive) }
-        else if (abt <= 57 && depth <= 50) { setDive('R', false, false); updateDive1(dive) }
-        else if (abt <= 60 && depth <= 50) { setDive('S', false, false); updateDive1(dive) }
-        else if (abt <= 63 && depth <= 50) { setDive('T', false, false); updateDive1(dive) }
-        else if (abt <= 67 && depth <= 50) { setDive('U', false, false); updateDive1(dive) }
-        else if (abt <= 71 && depth <= 50) { setDive('V', false, false); updateDive1(dive) }
-        else if (abt <= 75 && depth <= 50) { setDive('W', true, false); updateDive1(dive) }
-        else if (abt <= 80 && depth <= 50) { setDive('X', true, false); updateDive1(dive) }
+        else if (abt <= 7 && depth <= 50) { setAndUpdate('A', false, false, dive)}
+        else if (abt <= 13 && depth <= 50) { setAndUpdate('B', false, false, dive)}
+        else if (abt <= 17 && depth <= 50) { setAndUpdate('C', false, false, dive)}
+        else if (abt <= 19 && depth <= 50) { setAndUpdate('D', false, false, dive)}
+        else if (abt <= 21 && depth <= 50) { setAndUpdate('E', false, false, dive)}
+        else if (abt <= 24 && depth <= 50) { setAndUpdate('F', false, false, dive)}
+        else if (abt <= 26 && depth <= 50) { setAndUpdate('G', false, false, dive)}
+        else if (abt <= 28 && depth <= 50) { setAndUpdate('H', false, false, dive)}
+        else if (abt <= 31 && depth <= 50) { setAndUpdate('I', false, false, dive)}
+        else if (abt <= 33 && depth <= 50) { setAndUpdate('J', false, false, dive)}
+        else if (abt <= 36 && depth <= 50) { setAndUpdate('K', false, false, dive)}
+        else if (abt <= 39 && depth <= 50) { setAndUpdate('L', false, false, dive)}
+        else if (abt <= 41 && depth <= 50) { setAndUpdate('M', false, false, dive)}
+        else if (abt <= 44 && depth <= 50) { setAndUpdate('N', false, false, dive)}
+        else if (abt <= 47 && depth <= 50) { setAndUpdate('O', false, false, dive)}
+        else if (abt <= 50 && depth <= 50) { setAndUpdate('P', false, false, dive)}
+        else if (abt <= 53 && depth <= 50) { setAndUpdate('Q', false, false, dive)}
+        else if (abt <= 57 && depth <= 50) { setAndUpdate('R', false, false, dive)}
+        else if (abt <= 60 && depth <= 50) { setAndUpdate('S', false, false, dive)}
+        else if (abt <= 63 && depth <= 50) { setAndUpdate('T', false, false, dive)}
+        else if (abt <= 67 && depth <= 50) { setAndUpdate('U', false, false, dive)}
+        else if (abt <= 71 && depth <= 50) { setAndUpdate('V', false, false, dive)}
+        else if (abt <= 75 && depth <= 50) { setAndUpdate('W', true, false, dive)}
+        else if (abt <= 80 && depth <= 50) { setAndUpdate('X', true, false, dive)}
     }
 
 
