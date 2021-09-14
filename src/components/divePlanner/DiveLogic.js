79edcf52ef3241be43e3d7c57c4b6bb76c003422
DArmstrong87 <lenzflare@gmail.com>
2021-09-14 17:35:34 -0500

    conditionals for getting pressure group is simplified

--- src/components/divePlanner/DiveLogic.js
+++ src/components/divePlanner/DiveLogic.js
@@ -2,25 +2,8 @@ import React, { useState } from "react";
 
 export const DiveStates = () => {
     const [dive0, setDive0] = useState({})
-    const diveZeroReset = ()=>{
-        setDive0({})
-    }
+    const [dive1, updateDive1] = useState()
 
-    // let dive1 = {
-    //     depth: '',
-    //     abt: 0,
-    //     pg: '',
-    //     ssRequired: false,
-    //     noDecoLimit: false
-    // }
-    const [dive1, setDive1] = useState({
-        depth: '',
-        abt: 0,
-        pg: '',
-        ssRequired: false,
-        noDecoLimit: false
-    })
-    console.log('dive1', dive1)
     const [dive2, setDive2] = useState({
         surfInt: 0,
         resNitroTime: 0,
@@ -43,166 +26,95 @@ export const DiveStates = () => {
     })
 
     console.log('dive0', dive0)
-
-    const getPGdepth35 = () => {
-        console.log('run getPGdepth35')
-        const dive = { ...dive0 }
-        const setPG = (letter) => dive.pg = letter;
-        const setSS = (boolean) => dive.ssRequired = boolean;
-        const setDeco = (boolean) => dive.noDecoLimit = boolean;
-            if(dive0.abt <= 10){
-                return setPG('A'), setSS(false), setDeco(false), setDive1(dive)}
-            else if(dive0.abt <= 19){return setPG('B')}
-            // (dive0.abt <= 10) ? (setPG('A'), setSS(false), setDeco(false))
-            // : (dive0.abt <= 19) ? setPG('B') : ''
-        // dive0.abt <= 19 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'B'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 25 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'C'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 29 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'D'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 32 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'E'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 36 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'F'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 40 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'G'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 44 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'H'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 48 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'I'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 52 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'J'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 57 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'K'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 62 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'L'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 67 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'M'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 73 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'N'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 79 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'O'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 85 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'P'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 92 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'Q'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 100 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'R'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 108 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'S'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 117 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'T'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 127 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'U'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 139 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'V'
-        //     dive.ssRequired = false
-        //     setDive1(dive)} :
-        // dive0.abt <= 152 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'W'
-        //     dive.ssRequired = true
-        //     setDive1(dive)} :
-        // dive0.abt <= 168 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'X'
-        //     dive.ssRequired = true
-        //     setDive1(dive)} :
-        // dive0.abt <= 188 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'Y'
-        //     dive.ssRequired = true
-        //     setDive1(dive)} :
-        // dive0.abt >= 205 ? () => {
-        //     const dive = {...dive1}
-        //     dive.pg = 'Z'
-        //     dive.ssRequired = true
-        //     dive.noDecoLimit = true
-        //     setDive1(dive)} : ''
-
-        setDive1(dive)
-    };
+    console.log('dive1', dive1)
+    const dive = { ...dive0 }
+    const abt = dive.abt
+    const depth = dive.depth
+    const setPG = (letter) => dive.pg = letter;
+    const setSS = (boolean) => dive.ssRequired = boolean;
+    const setNDL = (boolean) => dive.noDecoLimit = boolean;
+    const setDive = (pg, ss, ndl) => { setPG(pg); setSS(ss); setNDL(ndl) };
 
     const getPressureGroup = () => {
-        console.log('run getPressureGroup')
-        return (
-            dive0.depth <= 35 ? getPGdepth35()
-                // :
-                // dive1.depth <= 40 && dive1.depth > 35 ? getPGdepth40() :
-                // dive1.depth <= 50 && dive1.depth > 40 ? getPGdepth50() :
-                // dive1.depth <= 60 && dive1.depth > 50 ? getPGdepth60() :
-                // dive1.depth <= 70 && dive1.depth > 60 ? getPGdepth70() :
-                // dive1.depth <= 80 && dive1.depth > 70 ? getPGdepth80() :
-                // dive1.depth <= 90 && dive1.depth > 80 ? getPGdepth90() :
-                // dive1.depth <= 100 && dive1.depth > 90 ? getPGdepth100() :
-                // dive1.depth <= 110 && dive1.depth > 100 ? getPGdepth110() :
-                // dive1.depth <= 120 && dive1.depth > 110 ? getPGdepth120() :
-                // dive1.depth <= 130 && dive1.depth > 120 ? getPGdepth130() :
-                // dive1.depth > 140 ? depth40() 
-                : ''
-        )
+        // Depth <= 35
+        if (abt <= 10 && depth <= 35) { setDive('A', false, false); updateDive1(dive) }
+        else if (abt <= 19 && depth <= 35) { setDive('B', false, false); updateDive1(dive) }
+        else if (abt <= 25 && depth <= 35) { setDive('C', false, false); updateDive1(dive) }
+        else if (abt <= 29 && depth <= 35) { setDive('D', false, false); updateDive1(dive) }
+        else if (abt <= 32 && depth <= 35) { setDive('E', false, false); updateDive1(dive) }
+        else if (abt <= 36 && depth <= 35) { setDive('F', false, false); updateDive1(dive) }
+        else if (abt <= 40 && depth <= 35) { setDive('G', false, false); updateDive1(dive) }
+        else if (abt <= 44 && depth <= 35) { setDive('H', false, false); updateDive1(dive) }
+        else if (abt <= 48 && depth <= 35) { setDive('I', false, false); updateDive1(dive) }
+        else if (abt <= 52 && depth <= 35) { setDive('J', false, false); updateDive1(dive) }
+        else if (abt <= 57 && depth <= 35) { setDive('K', false, false); updateDive1(dive) }
+        else if (abt <= 62 && depth <= 35) { setDive('L', false, false); updateDive1(dive) }
+        else if (abt <= 67 && depth <= 35) { setDive('M', false, false); updateDive1(dive) }
+        else if (abt <= 73 && depth <= 35) { setDive('N', false, false); updateDive1(dive) }
+        else if (abt <= 79 && depth <= 35) { setDive('O', false, false); updateDive1(dive) }
+        else if (abt <= 85 && depth <= 35) { setDive('P', false, false); updateDive1(dive) }
+        else if (abt <= 92 && depth <= 35) { setDive('Q', false, false); updateDive1(dive) }
+        else if (abt <= 100 && depth <= 35) { setDive('R', false, false); updateDive1(dive) }
+        else if (abt <= 108 && depth <= 35) { setDive('S', false, false); updateDive1(dive) }
+        else if (abt <= 117 && depth <= 35) { setDive('T', false, false); updateDive1(dive) }
+        else if (abt <= 127 && depth <= 35) { setDive('U', false, false); updateDive1(dive) }
+        else if (abt <= 139 && depth <= 35) { setDive('V', false, false); updateDive1(dive) }
+        else if (abt <= 152 && depth <= 35) { setDive('W', true, false); updateDive1(dive) }
+        else if (abt <= 168 && depth <= 35) { setDive('X', true, false); updateDive1(dive) }
+        else if (abt <= 188 && depth <= 35) { setDive('Y', true, false); updateDive1(dive) }
+        else if (abt <= 205 && depth <= 35) { setDive('Z', true, true); updateDive1(dive) }
+        // Depth <= 40
+        else if (abt <= 9 && depth <= 40) { setDive('A', false, false); updateDive1(dive) }
+        else if (abt <= 16 && depth <= 40) { setDive('B', false, false); updateDive1(dive) }
+        else if (abt <= 22 && depth <= 40) { setDive('C', false, false); updateDive1(dive) }
+        else if (abt <= 25 && depth <= 40) { setDive('D', false, false); updateDive1(dive) }
+        else if (abt <= 27 && depth <= 40) { setDive('E', false, false); updateDive1(dive) }
+        else if (abt <= 31 && depth <= 40) { setDive('F', false, false); updateDive1(dive) }
+        else if (abt <= 34 && depth <= 40) { setDive('G', false, false); updateDive1(dive) }
+        else if (abt <= 37 && depth <= 40) { setDive('H', false, false); updateDive1(dive) }
+        else if (abt <= 40 && depth <= 40) { setDive('I', false, false); updateDive1(dive) }
+        else if (abt <= 44 && depth <= 40) { setDive('J', false, false); updateDive1(dive) }
+        else if (abt <= 48 && depth <= 40) { setDive('K', false, false); updateDive1(dive) }
+        else if (abt <= 51 && depth <= 40) { setDive('L', false, false); updateDive1(dive) }
+        else if (abt <= 55 && depth <= 40) { setDive('M', false, false); updateDive1(dive) }
+        else if (abt <= 60 && depth <= 40) { setDive('N', false, false); updateDive1(dive) }
+        else if (abt <= 64 && depth <= 40) { setDive('O', false, false); updateDive1(dive) }
+        else if (abt <= 69 && depth <= 40) { setDive('P', false, false); updateDive1(dive) }
+        else if (abt <= 74 && depth <= 40) { setDive('Q', false, false); updateDive1(dive) }
+        else if (abt <= 79 && depth <= 40) { setDive('R', false, false); updateDive1(dive) }
+        else if (abt <= 85 && depth <= 40) { setDive('S', false, false); updateDive1(dive) }
+        else if (abt <= 91 && depth <= 40) { setDive('T', false, false); updateDive1(dive) }
+        else if (abt <= 97 && depth <= 40) { setDive('U', false, false); updateDive1(dive) }
+        else if (abt <= 104 && depth <= 40) { setDive('V', false, false); updateDive1(dive) }
+        else if (abt <= 111 && depth <= 40) { setDive('W', true, false); updateDive1(dive) }
+        else if (abt <= 120 && depth <= 40) { setDive('X', true, false); updateDive1(dive) }
+        else if (abt <= 129 && depth <= 40) { setDive('Y', true, false); updateDive1(dive) }
+        else if (abt <= 140 && depth <= 40) { setDive('Z', true, true); updateDive1(dive) }
+        // Depth <= 50
+        else if (abt <= 7 && depth <= 50) { setDive('A', false, false); updateDive1(dive) }
+        else if (abt <= 13 && depth <= 50) { setDive('B', false, false); updateDive1(dive) }
+        else if (abt <= 17 && depth <= 50) { setDive('C', false, false); updateDive1(dive) }
+        else if (abt <= 19 && depth <= 50) { setDive('D', false, false); updateDive1(dive) }
+        else if (abt <= 21 && depth <= 50) { setDive('E', false, false); updateDive1(dive) }
+        else if (abt <= 24 && depth <= 50) { setDive('F', false, false); updateDive1(dive) }
+        else if (abt <= 26 && depth <= 50) { setDive('G', false, false); updateDive1(dive) }
+        else if (abt <= 28 && depth <= 50) { setDive('H', false, false); updateDive1(dive) }
+        else if (abt <= 31 && depth <= 50) { setDive('I', false, false); updateDive1(dive) }
+        else if (abt <= 33 && depth <= 50) { setDive('J', false, false); updateDive1(dive) }
+        else if (abt <= 36 && depth <= 50) { setDive('K', false, false); updateDive1(dive) }
+        else if (abt <= 39 && depth <= 50) { setDive('L', false, false); updateDive1(dive) }
+        else if (abt <= 41 && depth <= 50) { setDive('M', false, false); updateDive1(dive) }
+        else if (abt <= 44 && depth <= 50) { setDive('N', false, false); updateDive1(dive) }
+        else if (abt <= 47 && depth <= 50) { setDive('O', false, false); updateDive1(dive) }
+        else if (abt <= 50 && depth <= 50) { setDive('P', false, false); updateDive1(dive) }
+        else if (abt <= 53 && depth <= 50) { setDive('Q', false, false); updateDive1(dive) }
+        else if (abt <= 57 && depth <= 50) { setDive('R', false, false); updateDive1(dive) }
+        else if (abt <= 60 && depth <= 50) { setDive('S', false, false); updateDive1(dive) }
+        else if (abt <= 63 && depth <= 50) { setDive('T', false, false); updateDive1(dive) }
+        else if (abt <= 67 && depth <= 50) { setDive('U', false, false); updateDive1(dive) }
+        else if (abt <= 71 && depth <= 50) { setDive('V', false, false); updateDive1(dive) }
+        else if (abt <= 75 && depth <= 50) { setDive('W', true, false); updateDive1(dive) }
+        else if (abt <= 80 && depth <= 50) { setDive('X', true, false); updateDive1(dive) }
     }
 
 
@@ -210,7 +122,7 @@ export const DiveStates = () => {
         <div>
             <fieldset>
                 <label>Depth</label>
-                <input type="number" onChange={(event) => {
+                <input type="number" value={dive0.depth} onChange={(event) => {
                     const dive = { ...dive0 }
                     dive.depth = parseInt(event.target.value)
                     setDive0(dive)
@@ -218,13 +130,14 @@ export const DiveStates = () => {
             </fieldset>
             <fieldset>
                 <label>Time</label>
-                <input type="number" onChange={(event) => {
+                <input type="number" value={dive0.abt} onChange={(event) => {
                     const dive = { ...dive0 }
                     dive.abt = parseInt(event.target.value)
                     setDive0(dive)
-                }}/>min
+                }} />min
             </fieldset>
             <button onClick={getPressureGroup}>Dive</button>
+            <button onClick={() => setDive0({ depth: 0, abt: 0 })}>Reset</button>
         </div>
         <div className="diveFlagGreen">
             <img src="https://media.istockphoto.com/photos/scuba-flag-waving-picture-id498827225?k=20&m=498827225&s=612x612&w=0&h=fKrkr7R3hWEvan9KJp4hlV4qJMKcVDBFwqrqOsopaw0=" width="200" />
