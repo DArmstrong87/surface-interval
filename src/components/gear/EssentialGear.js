ff9aadf3e82b0720e01fd96a00f5e31ab0ca7cf5
DArmstrong87 <lenzflare@gmail.com>
2021-09-20 15:00:54 -0500

    the forms display user input after hiding and unhiding the form

--- src/components/gear/EssentialGear.js
+++ src/components/gear/EssentialGear.js
@@ -17,7 +17,7 @@ export const EssentialGear = ({ gearCopy, setNewGear }) => {
                 <>
                     <fieldset>
                         <label>BCD</label>
-                        <input type="BCD" onChange={(event) => {
+                        <input type="BCD" value={gearCopy.bcd} onChange={(event) => {
                             const copy = { ...gearCopy }
                             copy.bcd = event.target.value
                             setNewGear(copy)
@@ -25,7 +25,7 @@ export const EssentialGear = ({ gearCopy, setNewGear }) => {
                     </fieldset>
                     <fieldset>
                         <label>Regulator</label>
-                        <input type="Regulator" onChange={(event) => {
+                        <input type="Regulator" value={gearCopy.regulator} onChange={(event) => {
                             const copy = { ...gearCopy }
                             copy.regulator = event.target.value
                             setNewGear(copy)
@@ -33,7 +33,7 @@ export const EssentialGear = ({ gearCopy, setNewGear }) => {
                     </fieldset>
                     <fieldset>
                         <label>Octopus</label>
-                        <input type="Octopus" onChange={(event) => {
+                        <input type="Octopus" value={gearCopy.octopus} onChange={(event) => {
                             const copy = { ...gearCopy }
                             copy.octopus = event.target.value
                             setNewGear(copy)
@@ -41,7 +41,7 @@ export const EssentialGear = ({ gearCopy, setNewGear }) => {
                     </fieldset>
                     <fieldset>
                         <label>Mask</label>
-                        <input type="Mask" onChange={(event) => {
+                        <input type="Mask" value={gearCopy.mask} onChange={(event) => {
                             const copy = { ...gearCopy }
                             copy.mask = event.target.value
                             setNewGear(copy)
@@ -49,7 +49,7 @@ export const EssentialGear = ({ gearCopy, setNewGear }) => {
                     </fieldset>
                     <fieldset>
                         <label>Fins</label>
-                        <input type="Fins" onChange={(event) => {
+                        <input type="Fins" value={gearCopy.fins} onChange={(event) => {
                             const copy = { ...gearCopy }
                             copy.fins = event.target.value
                             setNewGear(copy)
@@ -57,7 +57,7 @@ export const EssentialGear = ({ gearCopy, setNewGear }) => {
                     </fieldset>
                     <fieldset>
                         <label>Boots</label>
-                        <input type="Boots" onChange={(event) => {
+                        <input type="Boots" value={gearCopy.boots} onChange={(event) => {
                             const copy = { ...gearCopy }
                             copy.boots = event.target.value
                             setNewGear(copy)
@@ -65,7 +65,7 @@ export const EssentialGear = ({ gearCopy, setNewGear }) => {
                     </fieldset>
                     <fieldset>
                         <label>Computer</label>
-                        <input type="Computer" onChange={(event) => {
+                        <input type="Computer" value={gearCopy.computer} onChange={(event) => {
                             const copy = { ...gearCopy }
                             copy.computer = event.target.value
                             setNewGear(copy)
@@ -73,7 +73,7 @@ export const EssentialGear = ({ gearCopy, setNewGear }) => {
                     </fieldset>
                     <fieldset>
                         <label>Console</label>
-                        <input type="Console" onChange={(event) => {
+                        <input type="Console" value={gearCopy.console} onChange={(event) => {
                             const copy = { ...gearCopy }
                             copy.console = event.target.value
                             setNewGear(copy)
@@ -81,7 +81,7 @@ export const EssentialGear = ({ gearCopy, setNewGear }) => {
                     </fieldset>
                     <fieldset>
                         <label>Exposure Suit</label>
-                        <input type="Exposure-Suit" onChange={(event) => {
+                        <input type="Exposure-Suit" value={gearCopy.exposureSuit} onChange={(event) => {
                             const copy = { ...gearCopy }
                             copy.exposureSuit = event.target.value
                             setNewGear(copy)
@@ -89,7 +89,7 @@ export const EssentialGear = ({ gearCopy, setNewGear }) => {
                     </fieldset>
                     <fieldset>
                         <label>Weight</label>
-                        <input type="number" onChange={(event) => {
+                        <input type="number" value={gearCopy.weight} onChange={(event) => {
                             const copy = { ...gearCopy }
                             copy.weight = event.target.value
                             setNewGear(copy)
@@ -97,7 +97,7 @@ export const EssentialGear = ({ gearCopy, setNewGear }) => {
                     </fieldset>
                     <fieldset>
                         <label>Tank</label>
-                        <input type="Tank" onChange={(event) => {
+                        <input type="Tank" value={gearCopy.tank} onChange={(event) => {
                             const copy = { ...gearCopy }
                             copy.tank = event.target.value
                             setNewGear(copy)
