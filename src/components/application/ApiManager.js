d529bb9a35d21f96e124d7eb18095d951a4c1665
DArmstrong87 <lenzflare@gmail.com>
2021-09-23 15:37:40 -0500

    fixing import of react for use state and use effect

--- src/components/application/ApiManager.js
+++ src/components/application/ApiManager.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react/cjs/react.development"
+import { useEffect, useState } from "react"
 
 // ---------------------------------- GETTERS ----------------------------------
 // --------- User
