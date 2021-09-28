d3e644fb1437ad0b2d5f331189e1943e9a6d2132
DArmstrong87 <lenzflare@gmail.com>
2021-09-28 15:45:26 -0500

    optimized styling for mobile

--- src/components/divePlanner/DivePlanner.js
+++ src/components/divePlanner/DivePlanner.js
@@ -18,11 +18,11 @@ export const DivePlanner = () => {
                 <h1>Plan the dive, dive the plan.</h1>
                 <p>
                     &nbsp;&nbsp;&nbsp;&nbsp;
-                    This dive planner uses the PADI Dive Table. It is designed for new Open Water students to practice their dive planning skills as well as anyone planning a single dive or multiple dives. The dive planner informs the diver if a planned dive is relatively safe regarding nitrogen exposure only.
+                    This dive planner uses the PADI Recreational Dive Planner. It is designed for new Open Water students to practice their dive planning skills as well as anyone planning a single dive or multiple dives. The dive planner informs the diver if a planned dive is relatively safe regarding nitrogen exposure only.
                 </p>
                 <p>
                     &nbsp;&nbsp;&nbsp;&nbsp;
-                    Other factors can increase risk of developing decompression illness beyond depth, time and ascent rate, including dehydration, thermal stress, exertion, general fitness, post-dive air travel, etc. It can NOT predict diver behavior, dive conditions or in any way ensure dive safety prior to or during a dive. Remember to use a reliable dive computer and always dive within your limits.
+                    Other factors can increase risk of developing decompression illness beyond depth, time and ascent rate, including dehydration, thermal stress, exertion, general fitness, post-dive air travel, etc. The dive planner can NOT predict diver behavior, dive conditions or in any way ensure dive safety prior to or during a dive. Remember to use a reliable dive computer and always dive within your limits.
                 </p>
             </section>
         </article>
@@ -41,11 +41,11 @@ export const DivePlanner = () => {
         </section>
 
         <section className="air-selectors">
-            <input type="radio" onClick={() => setAir('air')} checked={air === 'air'} />
+            <input type="radio" onChange={() => setAir('air')} checked={air === 'air'} />
             <label>Air</label>
-            <input type="radio" onClick={() => setAir('EANx32')} checked={air === 'EANx32'} />
+            <input type="radio" onChange={() => setAir('EANx32')} checked={air === 'EANx32'} />
             <label>EANx32</label>
-            <input type="radio" onClick={() => setAir('EANx36')} checked={air === 'EANx36'} />
+            <input type="radio" onChange={() => setAir('EANx36')} checked={air === 'EANx36'} />
             <label>EANx36</label>
         </section>
 
